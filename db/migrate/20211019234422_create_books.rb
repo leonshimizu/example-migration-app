80186cd21d81cf35fe6318cc764eebe44614f926
“leonshimizu” <lmshimizu@gmail.com>
2021-10-19 17:05:17 -0700

    ran rails db:rollback and then changed the model attributes and then ran rails db:migrate

--- db/migrate/20211019234422_create_books.rb
+++ db/migrate/20211019234422_create_books.rb
@@ -2,8 +2,8 @@ class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
       t.string :title
-      t.string :description
       t.string :isbn
+      t.integer :price
 
       t.timestamps
     end
